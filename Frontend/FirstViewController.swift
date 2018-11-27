fb0dc613712aa406be4660e367786eaecb356bd1
vernonchan <vernontchan@gmail.com>
2018-11-27 13:58:06 -0800

    Initial Commit

---
+++ Frontend/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Frontend
+//
+//  Created by Vernon Chan on 11/27/18.
+//  Copyright © 2018 Artizian. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Frontend/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Frontend
+//
+//  Created by Vernon Chan on 11/27/18.
+//  Copyright © 2018 Artizian. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
