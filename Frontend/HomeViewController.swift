d2ad5328e4eecaedc0208acbf729abcc3b5aab1a
rckrieger <rebecca.c.krieger@gmail.com>
2018-12-04 21:32:16 -0800

    removed container

--- Frontend/HomeViewController.swift
+++ Frontend/HomeViewController.swift
@@ -11,16 +11,34 @@ import UIKit
 //home screen
 class FirstViewController: UIViewController {
 
-  //  @IBOutlet weak var carousel: UICollectionView!
-    
+
     @IBOutlet weak var welcomeMessage: UILabel!
-    @IBOutlet weak var bannerContainer: UIView!
+    //@IBOutlet weak var bannerContainer: UIView!
+    @IBOutlet weak var banner: UICollectionView!
     
+    @IBOutlet weak var bannerLayout: UICollectionViewFlowLayout!
     @IBOutlet weak var fakeGraphHolder: UIImageView!
     //carousel.scrollDirection = .horizontal
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bannerQuotes.count
+    }
+    let bannerQuotes = [bannerQuote(message: "Listings with photos get 30% higher interation", textColor: UIColor.blue, backgroundColor: UIColor.green), bannerQuote(message: "Incorperate keywords in listings "),
+                        bannerQuote(message: "Artisans make up the core of the community")
+    ]
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = banner.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath) as! customBannerCell
+        // Do any custom modifications you your cell, referencing the outlets you defined in the Custom cell file.
+        cell.messageLabel.backgroundColor = bannerQuotes[indexPath.row].backgroundColor
+        cell.messageLabel.textColor = bannerQuotes[indexPath.row].textColor
+        //print(bannerQuotes[indexPath.row].message)
+        cell.messageLabel.text = bannerQuotes[indexPath.row].message
+        
+        return cell
+        
     }
 
 
