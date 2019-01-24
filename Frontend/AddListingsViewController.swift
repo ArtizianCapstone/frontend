ad1e3e5bb9eae8d2ecb598e9e413b955854ac1a9
Zane Ali <zaneali@Zanes-MacBook-Air.local>
2019-01-24 14:35:52 -0800

    Connected

--- Frontend/AddListingsViewController.swift
+++ Frontend/AddListingsViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class AddListingsViewController: UIViewController {
     @IBOutlet weak var itemName: UITextField!
     
+    @IBOutlet weak var cancel: UIButton!
     @IBOutlet weak var itemDescription: UITextView!
     
     @IBOutlet weak var itemPrice: UITextField!
@@ -27,38 +28,44 @@ class AddListingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func cancelButton(_ sender: Any) {
+         performSegue(withIdentifier: "CancelListingSegue", sender: cancel)
+        
+    }
     @IBAction func submitAction(_ sender: Any) {
+        print("TEST")
         if (itemDescription.text! == "" || itemPrice.text! == "" || itemName.text! == ""){
-            print("error")
+            print("Forms not filled: error")
+        }else{
+            var price = Float(itemPrice.text!) ?? 0
+            let json: [String: Any] = [ "name": itemName.text!,"description": itemDescription.text!, "price": price]
+            
+            guard let jsonData = try? JSONSerialization.data(withJSONObject: json) else {return}
+            
+            // create post request
+            guard let url = URL(string: "http://localhost:3000/listings") else {return}
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            // insert json data to the request
+            request.httpBody = jsonData
+            
+            let task = URLSession.shared
+            task.dataTask(with: request) { (data, response, error) in
+                if let response = response {
+                    print( response )
+                }
+                if let data = data {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data, options: [])
+                        print( json )
+                    }
+                    catch{
+                        print(error)
+                    }
+                }
+                }.resume()
         }
-        var price = Float(itemPrice.text!) ?? 0
-        let json: [String: Any] = [ "name": itemName.text!,"description": itemDescription.text!, "price": price]
-        
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: json) else {return}
-        
-        // create post request
-        guard let url = URL(string: "http://localhost:3000/listings") else {return}
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        // insert json data to the request
-        request.httpBody = jsonData
-        
-        let task = URLSession.shared
-        task.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print( response )
-            }
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print( json )
-                }
-                catch{
-                    print(error)
-                }
-            }
-            }.resume()
     }
     
     /*
