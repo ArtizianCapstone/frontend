c81b65a22a994ee9555faa9f7b054e67887be786
rckrieger <rebecca.c.krieger@gmail.com>
2019-02-07 13:10:34 -0800

    Starter pack

--- Frontend/LoginViewController.swift
+++ Frontend/LoginViewController.swift
@@ -8,30 +8,22 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+
+class ShippingViewController: UIViewController {
     
     var users = ["jkurtz": "5c00776e2f1dfe588f33138c",
                  "bfoote":  "5c01b47607170f9377b207bc"]
-    //"we will have a master password for now"
+    //"we will have a master password for now"]
     
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         //TODO: Get the database information using POSTMAN
         // Do any additional setup after loading the view.
     }
     var authenticated = false
-    @IBOutlet weak var loginButton: UIButton!
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
-    
-    @IBAction func loginAction(_ sender: Any) {
-        print(username.text ?? "")
-        print(password.text ?? "")
-        if (users[username.text!] != nil && password.text! == "Artizian1"){
-            authenticated = true
-            performSegue(withIdentifier: "loginSegue", sender: loginButton)
-        }
-    }
+   
     /*
     // MARK: - Navigation
 
